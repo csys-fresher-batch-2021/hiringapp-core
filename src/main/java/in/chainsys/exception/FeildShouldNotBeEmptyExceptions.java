ebd7184b60cb2cb4780586c0499985ebf7662a75
Nevilston <82449413+Nevilston@users.noreply.github.com>
2021-07-19 18:56:51 +0530

    Add files via upload

---
+++ src/main/java/in/chainsys/exception/FeildShouldNotBeEmptyExceptions.java
@@ -0,0 +1,9 @@
+package in.chainsys.exception;
+
+public class FeildShouldNotBeEmptyExceptions extends Exception {
+	static final long serialVersionUID = 1L;
+
+	public FeildShouldNotBeEmptyExceptions(String message) {
+		super(message);
+	}
+}

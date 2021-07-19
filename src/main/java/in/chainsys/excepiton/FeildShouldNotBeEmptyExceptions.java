753137c47fa4f5f15c0699768ce5cb5afd189c9b
Nevilston <82449413+Nevilston@users.noreply.github.com>
2021-07-19 18:47:50 +0530

    Delete src directory

--- src/main/java/in/chainsys/excepiton/FeildShouldNotBeEmptyExceptions.java
+++
@@ -1,9 +0,0 @@
-package in.chainsys.excepiton;
-
-public class FeildShouldNotBeEmptyExceptions extends Exception {
-	static final long serialVersionUID = 1L;
-
-	public FeildShouldNotBeEmptyExceptions(String message) {
-		super(message);
-	}
-}
--- src/main/java/in/chainsys/exception/FeildShouldNotBeEmptyExceptions.java
+++
@@ -1,9 +0,0 @@
-package in.chainsys.exception;
-
-public class FeildShouldNotBeEmptyExceptions extends Exception {
-	static final long serialVersionUID = 1L;
-
-	public FeildShouldNotBeEmptyExceptions(String message) {
-		super(message);
-	}
-}
